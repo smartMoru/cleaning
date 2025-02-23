723fed63cef902b88369a99917d548e6a9b35408
Winni-Zhang <winni435496193@gmail.com>
2025-02-23 15:02:35 +0800

    image cal

--- client/src/RoutineCleaning.js
+++ client/src/RoutineCleaning.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Card } from 'antd';
-
+import { Carousel, Card } from 'antd';
+import './App.css';
+import r1 from './img/r1.jpg'
 const RoutineCleaning = () => {
   const data = [
-    { title: "服务1", description: "这是服务1的描述", imageUrl: "image1.jpg" },
+    { title: "服务1", description: "这是服务1的描述", imageUrl: r1 },
     { title: "服务2", description: "这是服务2的描述", imageUrl: "image2.jpg" },
     { title: "服务3", description: "这是服务3的描述", imageUrl: "image3.jpg" },
   ];
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    <Carousel autoplay style={{ width: '100%', padding: '20px' }}>
       {data.map((item, index) => (
-        <Card
-          key={index}
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt={item.title} src={item.imageUrl} />}
-        >
-          <Card.Meta title={item.title} description={item.description} />
-        </Card>
+        <div key={index}>
+          <Card
+            hoverable
+            style={{ width: '100%' }}
+            cover={<img alt={item.title} src={item.imageUrl} style={{ width: '100%', maxHeight: '400px', objectFit: 'cover' }} />}
+          >
+            <Card.Meta title={item.title} description={item.description} />
+          </Card>
+        </div>
       ))}
-    </div>
+    </Carousel>
   );
 };
 
